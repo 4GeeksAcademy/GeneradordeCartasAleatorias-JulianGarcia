32a42bdef0eb867b786e2e51470e48884026ea99
juliangarciag05 <juliangarciag05@gmail.com>
2025-10-06 03:51:40 +0000

    añadi boton

--- src/app.js
+++ src/app.js
@@ -8,34 +8,50 @@ import "./assets/img/4geeks.ico";
 window.onload = function () {
 
     let suits = ["♣", "♦", "♥", "♠"];
-    let numbers = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K"]
+    let numbers = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
     let colors = ["black", "red"];
 
+    function randomCards() {
 
-    let randomSuits = Math.floor(Math.random() * suits.length)
-    let randomNumbers = Math.floor(Math.random() * numbers.length)
-    let randomColors = Math.floor(Math.random() * colors.length)
+        let randomSuits = Math.floor(Math.random() * suits.length)
+        let randomNumbers = Math.floor(Math.random() * numbers.length)
+        let randomColors = Math.floor(Math.random() * colors.length)
+
+        document.getElementById("suit").innerText = suits[randomSuits];
+        document.getElementById("suitFooter").innerText = suits[randomSuits];
+        document.getElementById("number").innerText = numbers[randomNumbers];
+
+        let changeColorSuit = document.getElementById("suit")
+        let changeColorSuitFooter = document.getElementById("suitFooter")
+        let changeColorNumber = document.getElementById("number")
+
+        if (colors[randomColors] == "red") {
+            changeColorSuit.style.color = "red"
+            changeColorSuitFooter.style.color = "red"
+            changeColorNumber.style.color = "red"
+        } else {
+            changeColorSuit.style.color = "black"
+            changeColorSuitFooter.style.color = "black"
+            changeColorNumber.style.color = "black"
+        }
+
+    }
+
+    randomCards();
 
 
-    document.getElementById("suit").innerText = suits[randomSuits];
-    document.getElementById("suitFooter").innerText = suits[randomSuits];
-    document.getElementById("number").innerText = numbers[randomNumbers];
-
-
-    let changeColorSuit = document.getElementById("suit")
-    let changeColorSuitFooter = document.getElementById("suitFooter")
-    let changeColorNumber = document.getElementById("number")
-
-    if (colors[randomColors] == "red") {
-        changeColorSuit.style.color = "red"
-        changeColorSuitFooter.style.color = "red"
-        changeColorNumber.style.color = "red"
- } 
-
-
-
-
+    document.getElementById("button").addEventListener("click", randomCards);
     
+ 
+
+
+
+
+
+
+
+
+
 
 
 
